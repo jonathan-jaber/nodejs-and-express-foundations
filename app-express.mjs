63271672ba6b6ffa4a99361473a4c032c36ea952
Jonathan Jaber <jonjjaber@gmail.com>
2025-10-03 10:43:26 -0700

    I installed the cookie parser library. Still trying to get this running tho.

--- app-express.mjs
+++ app-express.mjs
@@ -1,4 +1,6 @@
 import express from 'express'
+import { join } from 'node:path'
+import cookieParser from 'cookie-parser'
 
 const server = express()
 
@@ -8,4 +10,29 @@ function requestCallback(request, response){
 
 server.get('/', requestCallback)
 
+app.get('/simple-text', (request, response) => {
+    console.log(`this is some text`)
+    response.status(200).set({ 'Content-Type': 'text/plain' }).send(`this is some text`)
+})
+
 server.listen(3000)
+
+    request.on('end', () => {
+        const parsedBody = JSON.parse(body)
+
+        let concert_id = parsedBody.concert_id
+        let concert_artist = parsedBody.concert_artist
+        let concert_price = parsedBody.concert_price
+        let concert_currency = parsedBody.concert_currency
+
+        let cart
+        if(request.cookies.cart === undefined) {
+            cart = []
+        } else {
+            cart = JSON.parse(request.cookies.cart)
+        }
+        cart.push({ concert_id, concert_artist, concert_price, concert_currency })
+
+        response.cookie('cart', JSON.stringify(cart))
+        response.status(200).send()
+    })
